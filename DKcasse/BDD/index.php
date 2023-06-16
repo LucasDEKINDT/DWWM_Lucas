200a6a51e686a727bcfb0d06c23e3b5b5eb9e3be
LucasDEKINDT <120390245+LucasDEKINDT@users.noreply.github.com>
2023-06-16 11:50:19 +0200

    ajout img et actualisation

--- DKcasse/BDD/index.php
+++ DKcasse/BDD/index.php
@@ -29,40 +29,71 @@ while ($donnees = $requete->fetch(PDO::FETCH_ASSOC)) // le while permet de boucl
 }
 var_dump($piece);
 
-/********************************************/
-/*****          REQUETE SIMPLE          *****/
-/********************************************/
+
 
 
 // $typepiece1 = new Typepiece(1,'mecanique rond',1);
-
+$requete = $db->query("SELECT * FROM typepiece "); // $requete est un objet de type PDO_Statement
+while ($donnees = $requete->fetch(PDO::FETCH_ASSOC)) // le while permet de boucler sur les enregistrements
+// il s'arrete quand fetch renvoi false
+{
+    var_dump($donnees);
+    $typepiece[] = new Typepiece($donnees);
+}
+var_dump($typepiece);
 // $typepiece1->affichagetypepiece();
 
 // echo "<br> <br>";
 
 // $modele1 = new Modele(1,"clio",2009);
-
+$requete = $db->query("SELECT * FROM modeles "); // $requete est un objet de type PDO_Statement
+while ($donnees = $requete->fetch(PDO::FETCH_ASSOC)) // le while permet de boucler sur les enregistrements
+// il s'arrete quand fetch renvoi false
+{
+    var_dump($donnees);
+    $modele[] = new Modele($donnees);
+}
+var_dump($modele);
 // $modele1->affichagemodele();
 
 // echo "<br> <br>";
 
 // $marque1 = new Marque(1,'renault',1);
-
+$requete = $db->query("SELECT * FROM marques"); // $requete est un objet de type PDO_Statement
+while ($donnees = $requete->fetch(PDO::FETCH_ASSOC)) // le while permet de boucler sur les enregistrements
+// il s'arrete quand fetch renvoi false
+{
+    var_dump($donnees);
+    $marque[] = new Marque($donnees);
+}
+var_dump($marque);
 // $marque1->affichagemarque();
 
 // echo "<br> <br>";
 
 // $correspondre1 = new Correspondre(1,1);
-
+$requete = $db->query("SELECT * FROM correspondre "); // $requete est un objet de type PDO_Statement
+while ($donnees = $requete->fetch(PDO::FETCH_ASSOC)) // le while permet de boucler sur les enregistrements
+// il s'arrete quand fetch renvoi false
+{
+    var_dump($donnees);
+    $correspondre[] = new Correspondre($donnees);
+}
+var_dump($correspondre);
 // $correspondre1->affichagecorres();
 
 // echo "<br> <br>";
 
 // $utilisateur1 = new Utilisateur(1,true,'dekindt','lucas','lucas.dekindt@ahoo.fr','azertyuiop789');
 
-/********************************************/
-/*****          REQUETE SIMPLE          *****/
-/********************************************/
+$requete = $db->query("SELECT * FROM utilisateur "); // $requete est un objet de type PDO_Statement
+while ($donnees = $requete->fetch(PDO::FETCH_ASSOC)) // le while permet de boucler sur les enregistrements
+// il s'arrete quand fetch renvoi false
+{
+    var_dump($donnees);
+    $utilisateur[] = new Utilisateur($donnees);
+}
+var_dump($utilisateur);
 
 
 // $utilisateur1->affichageutilisateur();
